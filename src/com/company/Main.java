62c5197142240c1c8eb93a0ca29688644e7f0abc
sincanmaulanaa <sincanmaulanaa@gmail.com>
2022-07-12 16:40:33 +0700

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+	// Rooms method
+	public static void rooms(int rooms) {
+		// Room data
+
+	}
+
+	public static void menus() {
+		System.out.println("========= Sistem Informasi Perhotelan =========");
+		System.out.println("1. Data Kamar");
+		System.out.println("2. Data Penyewa");
+		System.out.println("3. Data Transaksi");
+	}
+
+	public static void main(String[] args) {
+		// write your code here
+		menus();
+
+	}
+}
